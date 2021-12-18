d0cf16894107c3d8720cb6e100a1f7b3e37abe3d
Alan <me@alan.codes>
2021-12-17 21:07:48 -0600

    Initial version of the API

---
+++ index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+import cheerio from "cheerio";
+
+const PORT = 8080;
+const CORS_ORIGIN = "http://localhost:3000";
+
+const app = express();
+const corsOptions: cors.CorsOptions = {
+  origin: CORS_ORIGIN,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.get("/status", async (req, res) => {
+  const resp = await axios.get("https://srcares.org/activation-level-status/");
+  const $ = await cheerio.load(resp.data);
+
+  const imageURL = $("div.entry-content figure img").attr("data-orig-file");
+
+  console.log(imageURL);
+
+  res.status(200).send({ imageURI: imageURL });
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `Server started at ${CORS_ORIGIN}:${PORT}. Press Ctrl+C to quit.`
+  );
+});
