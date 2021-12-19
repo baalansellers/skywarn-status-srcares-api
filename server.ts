0edd82bc6380b6abcfdf1c3670a4602ccec3f8e3
Alan <me@alan.codes>
2021-12-18 20:33:58 -0600

    support two statuses: skywarn and ares

--- server.ts
+++ server.ts
@@ -12,11 +12,22 @@ const corsOptions: cors.CorsOptions = {
 
 app.use(cors(corsOptions));
 
-app.get("/status", async (req, res) => {
-  const resp = await axios.get("https://srcares.org/activation-level-status/");
+app.get("/skywarn-status", async (req, res) => {
+  const resp = await axios.get("https://srcares.dreamhosters.com/activation/");
   const $ = await cheerio.load(resp.data);
 
-  const imageURL = $("div.entry-content figure img").attr("data-orig-file");
+  const imageURL = $("figure.skywarn-status img").attr("src");
+
+  //console.log(imageURL);
+
+  res.status(200).send({ imageURI: imageURL });
+});
+
+app.get("/ares-status", async (req, res) => {
+  const resp = await axios.get("https://srcares.dreamhosters.com/activation/");
+  const $ = await cheerio.load(resp.data);
+
+  const imageURL = $("figure.ares-status img").attr("src");
 
   //console.log(imageURL);
 
@@ -24,7 +35,5 @@ app.get("/status", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(
-    `Server started on port:${PORT}. Press Ctrl+C to quit.`
-  );
+  console.log(`Server started on port:${PORT}. Press Ctrl+C to quit.`);
 });
